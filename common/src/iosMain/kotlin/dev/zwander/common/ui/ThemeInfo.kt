b145f53497c7c3a1fb66cab8fdea8236d57e2af6
Zachary Wander <zachary.wander@gmail.com>
2023-06-01 14:40:55 -0400

    Add dark mode listener for iOS

--- common/src/iosMain/kotlin/dev/zwander/common/ui/ThemeInfo.kt
+++ common/src/iosMain/kotlin/dev/zwander/common/ui/ThemeInfo.kt
@@ -2,22 +2,37 @@
 
 package dev.zwander.common.ui
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.interop.LocalUIViewController
 import dev.icerock.moko.resources.compose.colorResource
 import dev.zwander.common.monet.ColorScheme
 import dev.zwander.resources.common.MR
+import kotlinx.cinterop.ExportObjCClass
 import kotlinx.cinterop.alloc
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
+import kotlinx.cinterop.readValue
 import kotlinx.cinterop.value
 import platform.CoreGraphics.CGFloat
 import platform.CoreGraphics.CGFloatVar
+import platform.CoreGraphics.CGRectZero
 import platform.UIKit.UIColor
+import platform.UIKit.UITraitCollection
+import platform.UIKit.UIUserInterfaceStyle
+import platform.UIKit.UIView
+import platform.UIKit.UIViewController
+import platform.UIKit.addSubview
 import platform.UIKit.backgroundColor
+import platform.UIKit.currentTraitCollection
+import platform.UIKit.removeFromSuperview
 import platform.UIKit.tintColor
 import platform.UIKit.window
 import kotlin.experimental.ExperimentalObjCRefinement
@@ -52,7 +67,26 @@ actual fun getThemeInfo(): ThemeInfo {
         }
     } ?: arrayOfNulls(4)
 
-    val dark = isSystemInDarkTheme()
+    var style: UIUserInterfaceStyle by remember {
+        mutableStateOf(UITraitCollection.currentTraitCollection.userInterfaceStyle)
+    }
+
+    val dark by remember {
+        derivedStateOf { style == UIUserInterfaceStyle.UIUserInterfaceStyleDark }
+    }
+
+    val viewController: UIViewController = LocalUIViewController.current
+    DisposableEffect(Unit) {
+        val view: UIView = viewController.view
+        val traitView = TraitView {
+            style = UITraitCollection.currentTraitCollection.userInterfaceStyle
+        }
+        view.addSubview(traitView)
+
+        onDispose {
+            traitView.removeFromSuperview()
+        }
+    }
 
     val colorScheme = ColorScheme(
         if (red != null && green != null && blue != null && alpha != null) {
@@ -80,4 +114,15 @@ actual fun getThemeInfo(): ThemeInfo {
     rv?.backgroundColor = uiColor
 
     return colors
+}
+
+// https://github.com/JetBrains/compose-multiplatform/issues/3213#issuecomment-1572378546
+@ExportObjCClass
+private class TraitView(
+    private val onTraitChanged: () -> Unit
+) : UIView(frame = CGRectZero.readValue()) {
+    override fun traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        onTraitChanged()
+    }
 }
