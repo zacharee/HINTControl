9e198c2e2566d646b91a6df555e91847e7d92a48
Zachary Wander <zachary.wander@gmail.com>
2023-06-07 19:38:16 -0400

    Work on Xcode Cloud build

--- iosApp/ci_scripts/ci_post_clone.sh
+++ iosApp/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# https://stackoverflow.com/questions/72578619/building-kotlin-multiplatform-app-with-xcode-cloud
+# https://stackoverflow.com/questions/72578619/building-kotlin-multiplatform-app-with-xcode-cloud
+
 brew install cocoapods
 
 curl -s "https://get.sdkman.io" | bash
 . "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 18.0.2-open
+sdk install java 17.0.7-open
 
 pod install
 
+# Actually has to be set in the Environment Variables section in Xcode Cloud
 export JAVA_HOME=/Users/local/.sdkman/candidates/java/current
