4e3de37e39f3fe626a55cf70bddd1c32705c0500
Zachary Wander <zachary.wander@gmail.com>
2024-06-19 18:38:33 -0400

    Add horizontal system bars padding for iOS

--- common/src/iosMain/kotlin/Main.ios.kt
+++ common/src/iosMain/kotlin/Main.ios.kt
@@ -1,5 +1,10 @@
 @file:Suppress("FunctionName", "unused")
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.zwander.common.App
@@ -7,5 +12,6 @@ import dev.zwander.common.App
 fun MainViewController() = ComposeUIViewController {
     App(
         modifier = Modifier,
+        fullPadding = WindowInsets.systemBars.only(WindowInsetsSides.Horizontal).asPaddingValues(),
     )
 }
