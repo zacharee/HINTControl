d4e9feb53f479b2def5c1cef2201c3bc3655b914
Zachary Wander <zachary.wander@gmail.com>
2024-07-09 19:18:47 -0400

    Make TextSwitch background transparent

--- common/src/commonMain/kotlin/dev/zwander/common/components/TextSwitch.kt
+++ common/src/commonMain/kotlin/dev/zwander/common/components/TextSwitch.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlin.experimental.ExperimentalObjCRefinement
 import kotlin.native.HiddenFromObjC
@@ -24,7 +25,10 @@ fun TextSwitch(
 ) {
     AnimatedCard(
         onClick = { onCheckedChange(!checked) },
-        colors = CardDefaults.cardColors(),
+        colors = CardDefaults.outlinedCardColors().copy(
+            containerColor = Color.Transparent,
+            disabledContentColor = Color.Transparent,
+        ),
         border = null,
         modifier = modifier,
         enabled = enabled,
