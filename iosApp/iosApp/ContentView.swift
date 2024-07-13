07325b793c7788ecac6dc07290b00a10817418e1
Zachary Wander <zachary.wander@gmail.com>
2024-07-13 19:11:54 -0400

    Remove unused View from ContentView

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -10,20 +10,6 @@ struct ComposeView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
-struct VisualEffectView: UIViewRepresentable {
-    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-        
-    }
-    
-    func makeUIView(context: Context) -> UIVisualEffectView {
-        let view = UIVisualEffectView()
-        
-        view.effect = UIBlurEffect(style: .dark)
-        
-        return view
-    }
-}
-
 struct ContentView: View {
     var body: some View {
         ZStack {
