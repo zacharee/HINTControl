0163caa5c8525190a1bae1835025a26a36d8f7ab
Zachary Wander <zachary.wander@gmail.com>
2024-07-31 00:06:54 -0400

    More SnapshotChart performance improvements

--- common/src/commonMain/kotlin/dev/zwander/common/components/SnapshotChart.kt
+++ common/src/commonMain/kotlin/dev/zwander/common/components/SnapshotChart.kt
@@ -35,10 +35,7 @@ import io.github.koalaplot.core.legend.LegendLocation
 import io.github.koalaplot.core.line.LinePlot
 import io.github.koalaplot.core.style.LineStyle
 import io.github.koalaplot.core.util.ExperimentalKoalaPlotApi
-import io.github.koalaplot.core.xygraph.FloatLinearAxisModel
-import io.github.koalaplot.core.xygraph.Point
-import io.github.koalaplot.core.xygraph.XYGraph
-import io.github.koalaplot.core.xygraph.rememberAxisStyle
+import io.github.koalaplot.core.xygraph.*
 import korlibs.platform.Platform
 import korlibs.time.DateTime
 import korlibs.time.seconds
@@ -49,28 +46,75 @@ import kotlin.native.HiddenFromObjC
 private const val VERTICAL_AXIS_PADDING = 2
 
 private data class ChartData(
-    val data: List<Point<Float, Float>>,
+    val data: List<Point<Long, Int>>,
     val color: Color,
     val legendLabel: StringResource,
-)
+) {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun XYGraphScope<Long, Int>.Plot() {
+    LinePlot(
+        data = data,
+        lineStyle = LineStyle(brush = SolidColor(color), strokeWidth = 1.dp),
+        symbol = {
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(
+                    spacingBetweenTooltipAndAnchor = 4.dp,
+                ),
+                state = rememberTooltipState(),
+                tooltip = {
+                    PlainTooltip(caretSize = TooltipDefaults.caretSize) {
+                        Text(
+                            text = it.y.toString(),
+                            fontSize = 12.sp,
+                            lineHeight = 12.sp,
+                        )
+                    }
+                },
+                enableUserInput = Platform.isAndroid || Platform.isIos,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(6.dp)
+                        .background(color = color, CircleShape)
+                        .hoverableElement {
+                            Box(
+                                modifier = Modifier
+                                    .background(MaterialTheme.colorScheme.surfaceVariant, CircleShape)
+                                    .padding(4.dp),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                Text(
+                                    text = it.y.toString(),
+                                    fontSize = 12.sp,
+                                    lineHeight = 12.sp,
+                                )
+                            }
+                        },
+                )
+            }
+        },
+    )
+}
+}
 
-private fun createPoint(time: Long, value: Number?, minX: Long): Point<Float, Float>? {
+private fun createPoint(time: Long, value: Number?, minX: Long): Point<Long, Int>? {
     return if (value == null) {
         null
     } else {
-        Point((time - minX).toFloat(), value.toFloat())
+        Point((time - minX), value.toInt())
     }
 }
 
 @OptIn(
     ExperimentalObjCRefinement::class, ExperimentalKoalaPlotApi::class,
-    ExperimentalMaterial3Api::class
 )
 @HiddenFromObjC
 @Composable
 fun SnapshotChart(
     modifier: Modifier = Modifier,
 ) {
+    val isLightText = LocalContentColor.current.luminance() > 0.5f
     val fullSnapshots by Storage.snapshots.updates.collectAsState(listOf())
 
     if (fullSnapshots.isNullOrEmpty()) {
@@ -80,46 +124,33 @@ fun SnapshotChart(
     var snapshots by remember {
         mutableStateOf(listOf<HistoricalSnapshot>())
     }
+    var minX by remember {
+        mutableStateOf(0L)
+    }
+    var maxX by remember {
+        mutableStateOf(1L)
+    }
+    var minY by remember {
+        mutableStateOf(0)
+    }
+    var maxY by remember {
+        mutableStateOf(1)
+    }
+    val chartDataItems = remember {
+        mutableStateMapOf<Int, ChartData>()
+    }
 
     LaunchedEffect(fullSnapshots) {
         val currentTime = DateTime.nowUnixMillisLong()
-        snapshots = fullSnapshots?.run {
-            val firstIndex = this.indexOfFirst { it.timeMillis >= (currentTime - 60000) }.coerceAtLeast(0)
-            slice(firstIndex..lastIndex)
+        val newSnapshots = fullSnapshots?.sortedBy { it.timeMillis }?.run {
+            val firstIndex = indexOfFirst { it.timeMillis >= (currentTime - (60 * 1000))}.takeIf { it != -1 }
+
+            firstIndex?.let { slice(firstIndex..lastIndex) } ?: takeLast(10)
         } ?: listOf()
-    }
 
-    if (snapshots.isEmpty()) {
-        return
-    }
-
-    val minX by remember {
-        derivedStateOf {
-            (snapshots.minOfOrNull { it.timeMillis } ?: 0)
-        }
-    }
-    val maxX by remember {
-        derivedStateOf {
-            (snapshots.maxOfOrNull { it.timeMillis } ?: 0)
-        }
-    }
-    val maxXDateTime = DateTime.fromUnixMillis(maxX)
-
-    val xAxisModel by remember {
-        derivedStateOf {
-            FloatLinearAxisModel(
-                range = if (minX == maxX) {
-                    0f..1f
-                } else {
-                    0f..(maxX - minX).toFloat()
-                },
-            )
-        }
-    }
-
-    val minY by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
+        minX = (newSnapshots.minOfOrNull { it.timeMillis } ?: 0)
+        maxX = (newSnapshots.maxOfOrNull { it.timeMillis } ?: 0)
+        minY = snapshots.mapNotNull { snapshot ->
                 nullableMinOf(
                     snapshot.mainData?.signal?.fiveG?.rsrp,
                     snapshot.mainData?.signal?.fiveG?.rsrq,
@@ -131,12 +162,7 @@ fun SnapshotChart(
                     snapshot.mainData?.signal?.fourG?.sinr,
                 )?.let { it - VERTICAL_AXIS_PADDING }
             }.minOrNull() ?: 0
-        }
-    }
-
-    val maxY by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
+        maxY = snapshots.mapNotNull { snapshot ->
                 nullableMaxOf(
                     snapshot.mainData?.signal?.fiveG?.rsrp,
                     snapshot.mainData?.signal?.fiveG?.rsrq,
@@ -148,138 +174,116 @@ fun SnapshotChart(
                     snapshot.mainData?.signal?.fourG?.sinr,
                 )?.let { it + VERTICAL_AXIS_PADDING }
             }.maxOrNull() ?: 1
-        }
+
+        snapshots = newSnapshots
+
+        chartDataItems[0] = ChartData(
+            data = newSnapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.rsrp, minX)
+            },
+            color = if (isLightText) Color(0xffbc8f8f) else Color(0xff3cb371),
+            legendLabel = MR.strings.chart_legend_5g_rsrp,
+        )
+        chartDataItems[1] = ChartData(
+            data = snapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.rsrq, minX)
+            },
+            color = if (isLightText) Color(0xff32cd32) else Color(0xff000080),
+            legendLabel = MR.strings.chart_legend_5g_rsrq,
+        )
+        chartDataItems[2] = ChartData(
+            data = snapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.rssi, minX)
+            },
+            color = if (isLightText) Color(0xffff4500) else Color(0xffbc8f8f),
+            legendLabel = MR.strings.chart_legend_5g_rssi,
+        )
+        chartDataItems[3] = ChartData(
+            data = snapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.sinr, minX)
+            },
+            color = if (isLightText) Color(0xffffd700) else Color(0xffb03060),
+            legendLabel = MR.strings.chart_legend_5g_sinr,
+        )
+        chartDataItems[4] = ChartData(
+            data = snapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.rsrp, minX)
+            },
+            color = if (isLightText) Color(0xff00ffff) else Color(0xffff0000),
+            legendLabel = MR.strings.chart_legend_lte_rsrp,
+        )
+        chartDataItems[5] = ChartData(
+            data = snapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.rsrq, minX)
+            },
+            color = if (isLightText) Color(0xffa020f0) else Color(0xff2e8b57),
+            legendLabel = MR.strings.chart_legend_lte_rsrq,
+        )
+        chartDataItems[6] = ChartData(
+            data = snapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.rssi, minX)
+            },
+            color = if (isLightText) Color(0xff1e90ff) else Color(0xffb03060),
+            legendLabel = MR.strings.chart_legend_lte_rssi,
+        )
+        chartDataItems[7] = ChartData(
+            data = snapshots.mapNotNull { snapshot ->
+                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.sinr, minX)
+            },
+            color = if (isLightText) Color(0xffff1493) else Color(0xff00bfff),
+            legendLabel = MR.strings.chart_legend_lte_sinr,
+        )
     }
 
-    val yAxisModel by remember {
+    if (snapshots.isEmpty()) {
+        return
+    }
+
+    val maxXDateTime = DateTime.fromUnixMillis(maxX)
+
+    val xAxisModel by remember {
         derivedStateOf {
-            FloatLinearAxisModel(
-                range = if (minY == maxY) {
-                    0f..1f
+            LongLinearAxisModel(
+                range = if (minX == maxX) {
+                    0L..1L
                 } else {
-                    minY.toFloat()..maxY.toFloat()
+                    0L..(maxX - minX)
                 },
             )
         }
     }
 
-    val isLightText = LocalContentColor.current.luminance() > 0.5f
-
-    val fiveGRsrp by remember {
+    val yAxisModel by remember {
         derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.rsrp, minX)
-            }
+            IntLinearAxisModel(
+                range = if (minY == maxY) {
+                    0..1
+                } else {
+                    minY..maxY
+                },
+            )
         }
     }
-    val fiveGRsrq by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.rsrq, minX)
-            }
-        }
-    }
-    val fiveGRssi by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.rssi, minX)
-            }
-        }
-    }
-    val fiveGSinr by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fiveG?.sinr, minX)
-            }
-        }
-    }
-    val fourGRsrp by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.rsrp, minX)
-            }
-        }
-    }
-    val fourGRsrq by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.rsrq, minX)
-            }
-        }
-    }
-    val fourGRssi by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.rssi, minX)
-            }
-        }
-    }
-    val fourGSinr by remember {
-        derivedStateOf {
-            snapshots.mapNotNull { snapshot ->
-                createPoint(snapshot.timeMillis, snapshot.mainData?.signal?.fourG?.sinr, minX)
-            }
-        }
-    }
-
-    val chartDataItems = listOf(
-        ChartData(
-            data = fiveGRsrp,
-            color = if (isLightText) Color(0xffbc8f8f) else Color(0xff3cb371),
-            legendLabel = MR.strings.chart_legend_5g_rsrp,
-        ),
-        ChartData(
-            data = fiveGRsrq,
-            color = if (isLightText) Color(0xff32cd32) else Color(0xff000080),
-            legendLabel = MR.strings.chart_legend_5g_rsrq,
-        ),
-        ChartData(
-            data = fiveGRssi,
-            color = if (isLightText) Color(0xffff4500) else Color(0xffbc8f8f),
-            legendLabel = MR.strings.chart_legend_5g_rssi,
-        ),
-        ChartData(
-            data = fiveGSinr,
-            color = if (isLightText) Color(0xffffd700) else Color(0xffb03060),
-            legendLabel = MR.strings.chart_legend_5g_sinr,
-        ),
-        ChartData(
-            data = fourGRsrp,
-            color = if (isLightText) Color(0xff00ffff) else Color(0xffff0000),
-            legendLabel = MR.strings.chart_legend_lte_rsrp,
-        ),
-        ChartData(
-            data = fourGRsrq,
-            color = if (isLightText) Color(0xffa020f0) else Color(0xff2e8b57),
-            legendLabel = MR.strings.chart_legend_lte_rsrq,
-        ),
-        ChartData(
-            data = fourGRssi,
-            color = if (isLightText) Color(0xff1e90ff) else Color(0xffb03060),
-            legendLabel = MR.strings.chart_legend_lte_rssi,
-        ),
-        ChartData(
-            data = fourGSinr,
-            color = if (isLightText) Color(0xffff1493) else Color(0xff00bfff),
-            legendLabel = MR.strings.chart_legend_lte_sinr,
-        ),
-    )
 
     ChartLayout(
         legend = {
             FlowLegend(
                 itemCount = chartDataItems.size,
                 symbol = {
-                    Box(
-                        modifier = Modifier.size(12.dp)
-                            .background(chartDataItems[it].color),
-                    )
+                    chartDataItems[it]?.color?.let { color ->
+                        Box(
+                            modifier = Modifier.size(12.dp)
+                                .background(color),
+                        )
+                    }
                 },
                 label = {
-                    Text(
-                        text = stringResource(chartDataItems[it].legendLabel),
-                        style = MaterialTheme.typography.labelSmall,
-                    )
+                    chartDataItems[it]?.legendLabel?.let { label ->
+                        Text(
+                            text = stringResource(label),
+                            style = MaterialTheme.typography.labelSmall,
+                        )
+                    }
                 },
                 modifier = Modifier.padding(top = 8.dp),
             )
@@ -291,7 +295,7 @@ fun SnapshotChart(
             xAxisModel = xAxisModel,
             yAxisModel = yAxisModel,
             xAxisLabels = {
-                val seconds = (maxXDateTime - DateTime.fromUnixMillis(it.toLong() + minX)).seconds
+                val seconds = (maxXDateTime - DateTime.fromUnixMillis(it + minX)).seconds
                 Text(
                     text = MR.strings.graph_seconds_format(seconds.roundToInt()),
                     color = MaterialTheme.colorScheme.onBackground,
@@ -304,7 +308,7 @@ fun SnapshotChart(
             },
             yAxisLabels = {
                 Text(
-                    text = it.roundToInt().toString(),
+                    text = it.toString(),
                     color = MaterialTheme.colorScheme.onBackground,
                     style = MaterialTheme.typography.bodySmall.copy(
                         fontSize = 12.sp,
@@ -318,49 +322,8 @@ fun SnapshotChart(
             panZoomEnabled = false,
             yAxisStyle = rememberAxisStyle(labelRotation = 90),
         ) {
-            chartDataItems.forEach { chartData ->
-                LinePlot(
-                    data = chartData.data,
-                    lineStyle = LineStyle(brush = SolidColor(chartData.color), strokeWidth = 1.dp),
-                    symbol = {
-                        TooltipBox(
-                            positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(
-                                spacingBetweenTooltipAndAnchor = 4.dp,
-                            ),
-                            state = rememberTooltipState(),
-                            tooltip = {
-                                PlainTooltip(caretSize = TooltipDefaults.caretSize) {
-                                    Text(
-                                        text = it.y.toString(),
-                                        fontSize = 12.sp,
-                                        lineHeight = 12.sp,
-                                    )
-                                }
-                            },
-                            enableUserInput = Platform.isAndroid || Platform.isIos,
-                        ) {
-                            Box(
-                                modifier = Modifier
-                                    .size(6.dp)
-                                    .background(color = chartData.color, CircleShape)
-                                    .hoverableElement {
-                                        Box(
-                                            modifier = Modifier
-                                                .background(MaterialTheme.colorScheme.surfaceVariant, CircleShape)
-                                                .padding(4.dp),
-                                            contentAlignment = Alignment.Center,
-                                        ) {
-                                            Text(
-                                                text = it.y.toString(),
-                                                fontSize = 12.sp,
-                                                lineHeight = 12.sp,
-                                            )
-                                        }
-                                    },
-                            )
-                        }
-                    },
-                )
+            chartDataItems.values.forEach { item ->
+                with (item) { Plot() }
             }
         }
     }
