602ebbd4bcb4f3433996ef35ccfe8dfb636245f4
Zachary Wander <zachary.wander@gmail.com>
2024-08-07 22:43:43 -0400

    Update dependencies and remove dead native macOS code

--- common/src/macosMain/kotlin/dev/zwander/common/util/SettingsManager.kt
+++
@@ -1,8 +0,0 @@
-package dev.zwander.common.util
-
-import com.russhwolf.settings.NSUserDefaultsSettings
-import com.russhwolf.settings.Settings
-
-actual fun ActualSettings(): Settings {
-    return NSUserDefaultsSettings.Factory().create("group.dev.zwander.arcadyankvd21control.group")
-}
