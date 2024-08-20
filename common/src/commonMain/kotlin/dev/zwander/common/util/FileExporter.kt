22ed436680e36229c63f7f757d153555dfc5f324
Zachary Wander <zachary.wander@gmail.com>
2024-08-20 14:06:34 -0400

    Fix file exports on iOS

--- common/src/commonMain/kotlin/dev/zwander/common/util/FileExporter.kt
+++ common/src/commonMain/kotlin/dev/zwander/common/util/FileExporter.kt
@@ -2,6 +2,8 @@ package dev.zwander.common.util
 
 import io.github.vinceglb.filekit.core.FileKit
 import io.github.vinceglb.filekit.core.PlatformFile
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okio.BufferedSink
 
 object FileExporter {
@@ -10,7 +12,9 @@ object FileExporter {
         val baseName = fileName.slice(0 until dotIndex)
         val extension = fileName.slice(dotIndex + 1 until fileName.length)
 
-        val result = FileKit.saveFile(baseName = baseName, extension = extension)
+        val result = withContext(Dispatchers.Main) {
+            FileKit.saveFile(baseName = baseName, extension = extension)
+        }
 
         return result?.bufferedSink(append)
     }
