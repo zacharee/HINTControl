94bf468b5d780d0e977f3fe239997510522a5e63
Zachary Wander <zachary.wander@gmail.com>
2025-06-15 16:10:02 -0400

    Version 1.14.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,14 @@
 @file:Suppress("UnstableApiUsage")
 
+import com.android.build.api.instrumentation.AsmClassVisitorFactory
+import com.android.build.api.instrumentation.ClassContext
+import com.android.build.api.instrumentation.ClassData
+import com.android.build.api.instrumentation.InstrumentationParameters
+import com.android.build.api.instrumentation.InstrumentationScope
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.FieldVisitor
+
+
 plugins {
     alias(libs.plugins.compose)
     alias(libs.plugins.android.application)
@@ -55,3 +64,52 @@ dependencies {
     implementation(project(":common"))
     coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
+
+// Workaround for ktor 3.2.0.
+// https://youtrack.jetbrains.com/issue/KTOR-8583/Space-characters-in-SimpleName-error-when-executing-R8-mergeExtDex-task-with-3.2.0
+androidComponents {
+    onVariants { variant ->
+        variant.instrumentation.transformClassesWith(
+            FieldSkippingClassVisitor.Factory::class.java,
+            scope = InstrumentationScope.ALL,
+        ) { params ->
+            params.classes.add("io.ktor.client.plugins.Messages")
+        }
+    }
+}
+
+class FieldSkippingClassVisitor(
+    apiVersion: Int,
+    nextClassVisitor: ClassVisitor,
+) : ClassVisitor(apiVersion, nextClassVisitor) {
+
+    // Returning null from this method will cause the ClassVisitor to strip all fields from the class.
+    override fun visitField(
+        access: Int,
+        name: String?,
+        descriptor: String?,
+        signature: String?,
+        value: Any?
+    ): FieldVisitor? = null
+
+    abstract class Factory : AsmClassVisitorFactory<Parameters> {
+        private val excludedClasses
+            get() = parameters.get().classes.get()
+
+        override fun isInstrumentable(classData: ClassData): Boolean =
+            classData.className in excludedClasses
+
+        override fun createClassVisitor(classContext: ClassContext, nextClassVisitor: ClassVisitor): ClassVisitor {
+            return FieldSkippingClassVisitor(
+                apiVersion = instrumentationContext.apiVersion.get(),
+                nextClassVisitor = nextClassVisitor,
+            )
+        }
+    }
+
+    abstract class Parameters : InstrumentationParameters {
+        @get:Input
+        abstract val classes: SetProperty<String>
+    }
+}
+
--- common/common.podspec
+++ common/common.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
     spec.name                     = 'common'
-    spec.version                  = '52'
+    spec.version                  = '53'
     spec.homepage                 = 'https://zwander.dev'
     spec.source                   = { :http=> ''}
     spec.authors                  = ''
