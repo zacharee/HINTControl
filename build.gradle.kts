0e8a893cf2657359f0cd3ed72b413c4e66f490b7
Zachary Wander <zachary.wander@gmail.com>
2025-06-20 20:00:13 -0400

    Version 1.14.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ val androidTargetSdk by extra(34)
 val androidMinSdk by extra(24)
 val javaVersion by extra(JavaVersion.VERSION_21)
 
-val appVersionCode by extra(53)
-val appVersionName by extra("1.14.0")
+val appVersionCode by extra(54)
+val appVersionName by extra("1.14.1")
 
 val appGroup by extra("dev.zwander")
 val appPackageName by extra("dev.zwander.arcadyankvd21control")
--- common/common.podspec
+++ common/common.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
     spec.name                     = 'common'
-    spec.version                  = '53'
+    spec.version                  = '54'
     spec.homepage                 = 'https://zwander.dev'
     spec.source                   = { :http=> ''}
     spec.authors                  = ''
