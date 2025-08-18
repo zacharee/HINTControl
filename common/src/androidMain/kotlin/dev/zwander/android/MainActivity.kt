7a8d8aefdc3f315020a048440dd2d543209746a1
Zachary Wander <zachary.wander@gmail.com>
2025-08-17 20:41:57 -0400

    Remove LocalOrientation on Android

--- common/src/androidMain/kotlin/dev/zwander/android/MainActivity.kt
+++ common/src/androidMain/kotlin/dev/zwander/android/MainActivity.kt
@@ -5,8 +5,6 @@ import android.content.ComponentName
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.view.Display
-import android.view.Surface
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,15 +15,12 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import dev.zwander.common.App
 import dev.zwander.common.model.SettingsModel
-import dev.zwander.common.ui.LocalOrientation
-import dev.zwander.common.ui.Orientation
 import dev.zwander.common.widget.ConnectionStatusWidgetReceiver
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.dialogs.init
@@ -55,34 +50,15 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             val widgetRefresh by SettingsModel.widgetRefresh.collectAsState()
-            val display: Display? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                display
-            } else {
-                @Suppress("DEPRECATION")
-                windowManager.defaultDisplay
-            }
-            val orientation = display?.rotation.run {
-                when (this) {
-                    Surface.ROTATION_0 -> Orientation.PORTRAIT
-                    Surface.ROTATION_90 -> Orientation.LANDSCAPE_90
-                    Surface.ROTATION_270 -> Orientation.LANDSCAPE_270
-                    Surface.ROTATION_180 -> Orientation.PORTRAIT_180
-                    else -> error("Invalid orientation $this")
-                }
-            }
 
             LaunchedEffect(widgetRefresh) {
                 updateWidgetRefresh()
             }
 
-            CompositionLocalProvider(
-                LocalOrientation provides orientation,
-            ) {
-                App(
-                    modifier = Modifier.imePadding(),
-                    fullPadding = WindowInsets.systemBars.only(WindowInsetsSides.Horizontal).asPaddingValues(),
-                )
-            }
+            App(
+                modifier = Modifier.imePadding(),
+                fullPadding = WindowInsets.systemBars.only(WindowInsetsSides.Horizontal).asPaddingValues(),
+            )
         }
     }
 
