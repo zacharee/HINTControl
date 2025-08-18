b9496995c9556b2363b99854a18638e1e236e6b9
Zachary Wander <zachary.wander@gmail.com>
2025-08-17 20:07:23 -0400

    Bump compileSdk and targetSdk to 36

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-val androidCompileSdk by extra(35)
-val androidTargetSdk by extra(34)
+val androidCompileSdk by extra(36)
+val androidTargetSdk by extra(36)
 val androidMinSdk by extra(24)
 val javaVersion by extra(JavaVersion.VERSION_21)
 
